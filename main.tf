16296bad6cdaad4cc1804a13e495e23b0399e2b8
Keith Rhea <keithr@mindpointgroup.com>
2020-11-24 16:25:30 -0700

    Fix interpolation syntax.

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "config_sns_access_policy" {
       variable = "AWS:SourceOwner"
 
       values = [
-        "${var.sns_topic_owner}",
+        "var.sns_topic_owner",
       ]
     }
   }
